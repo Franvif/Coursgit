30c8188981111c15fe604cc7af578667e6bb883e
Fran Vif <57331799+Franvif@users.noreply.github.com>
2019-11-10 19:26:20 +0100

    Prise en compte du cas de la division par zéro

--- quotient.m
+++ quotient.m
@@ -1,6 +1,11 @@
 function quotient(a,b)
   % fonction qui écrit sur l'écran le quotient des deux entrées
   
-  s = a / b;
-  fprintf('%d / %d = %f\n',a,b,s);
+  if b==0
+    fprintf('Division par zéro, résultat indéfini.\n')
+  else
+    s = a / b;
+    fprintf('%d / %d = %f\n',a,b,s);
+  end
+  
   
