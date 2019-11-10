e1e1173c2c6f0d36a83f5580fb2ee2c11922d57a
Fran Vif <57331799+Franvif@users.noreply.github.com>
2019-11-10 18:32:04 +0100

    Ajout du readme et de l'ébauche de la fonction principale.

---
+++ README.txt
@@ -0,0 +1,3 @@
+Cours OpenClassRooms Git&GitHub
+
+Ecriture en Matlab de fonctions arithmétiques sur des entiers avec présentation du résultat.
---
+++ main.m
@@ -0,0 +1,7 @@
+function main
+% fonction principale
+% Demande deux entiers et une opération à réaliser
+
+a = input('Entrez un premier nombre entier: ');
+b = input('Entrez un second nombre entier: ');
+op = input('Quelle opération voulez-vous réaliser ? (+ ou - ou / ou *): ','s');
