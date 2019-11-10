0cb8bee56ad937a400d616f69b4f1b2be9919ddb
Fran Vif <57331799+Franvif@users.noreply.github.com>
2019-11-10 19:01:03 +0100

    Squelette de la fonction main complété

--- main.m
+++ main.m
@@ -2,6 +2,29 @@ function main
 % fonction principale
 % Demande deux entiers et une opération à réaliser
 
-a = input('Entrez un premier nombre entier: ');
-b = input('Entrez un second nombre entier: ');
+% init
+a = [];
+b = [];
+
+while isempty(a) || ~isnumeric(a)
+  a = input('Entrez un premier nombre entier: ');
+end
+
+while isempty (b) || ~isnumeric(b)
+  b = input('Entrez un second nombre entier: ');
+end
+
 op = input('Quelle opération voulez-vous réaliser ? (+ ou - ou / ou *): ','s');
+
+switch op
+  case '+'
+    fprintf('add');
+  case '-'
+    fprintf('sub');
+  case '*'
+    fprintf('multi');
+  case '/'
+    fprintf('div');
+  otherwise
+    error('Opération inconnue !');
+end
